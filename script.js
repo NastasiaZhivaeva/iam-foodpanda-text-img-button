c2fa5ff6a05a33dad384fef8027c8472d80c5f45
Nastasia Zhivaeva <nastasia.zhivaeva@d6v39qynyx.ad.deliveryhero.com>
2024-07-16 17:18:22 +0200

    updates

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 $(document).ready(function(){
-    $('.slick-container').slick({
-        dots: true,
-        swipe: true,
-        infinite: false,
-        customPaging: function(slider, i) {
-            return '<button class="slick-dot"></button>';
-        },
-        appendDots: $('.slick-dots'),
-        prevArrow: $('.slick-prev'),
-        nextArrow: $('.slick-next')
-    });
-  
-    function adjustDotSpacing() {
+  $('.slick-container').slick({
+      dots: true,
+      swipe: true,
+      infinite: false,
+      accessibility: true,
+      cssEase: 'ease',
+      adaptiveHeight: true,
+      customPaging: function(slider, i) {
+          return '<button class="slick-dot"></button>';
+      },
+      appendDots: $('.slick-dots'),
+      prevArrow: $('.slick-prev'),
+      nextArrow: $('.slick-next')
+  });
+
+  function adjustDotSpacing() {
       var dots = $('.slick-dots li');
       dots.each(function() {
-        var button = $(this).find('button');
-        if ($(this).hasClass('slick-active')) {
-          button.css('width', '48px');
-        } else {
-          button.css('width', '8px');
-        }
+          var button = $(this).find('button');
+          if ($(this).hasClass('slick-active')) {
+              button.css('width', '48px');
+          } else {
+              button.css('width', '8px'),
+              button.css('background-color', '--primary-color');
+          }
       });
-  
+
       dots.css('margin', '0 4px');
-      $('.slick-dots li.slick-active').css('margin', '0 12px');
-    }
-  
-    $('.slick-container').on('afterChange', function(event, slick, currentSlide){
+      $('.slick-dots li.slick-active').css('margin', '0 4px');
+  }
+
+  $('.slick-container').on('swipe', function(event, slick, direction){
       adjustDotSpacing();
-    });
-  
-    adjustDotSpacing(); // Initial call to set spacing on page load
   });
-  
+    $('.slick-prev, .slick-next').on('click', function() {
+        adjustDotSpacing();
+    });
+
+
+  adjustDotSpacing();
+});
